8d3df3016457f625115c27d97523ec2b102ba550
lucky <lucky@126.com>
2017-05-19 17:30:57 +0800

    add something

--- application/admin/controller/Bulletin.php
+++ application/admin/controller/Bulletin.php
@@ -3,34 +3,24 @@
  * Created by PhpStorm.
  * User: lucky
  * Date: 2017/5/19
- * Time: 17:15
+ * Time: 17:28
  */
 
 namespace app\admin\controller;
 
+
 /**
- * 学习情况控制器
+ * 论坛控制器
  * @package app\admin\controller
  */
-class Progress extends Basic
+class Bulletin extends Basic
 {
     /**
-     * 学习情况列表
+     * 论坛列表
      *
      * @return \think\Response
      */
     public function index()
-    {
-        //
-        return $this->fetch();
-    }
-
-    /**
-     * 学习情况统计
-     *
-     * @return \think\Response
-     */
-    public function Stat()
     {
         //
         return $this->fetch();
